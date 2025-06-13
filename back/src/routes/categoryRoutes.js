178c372e2ad989c2ba86e55f4e11368accc7538a
Sreypich183 <lyysreypich2@gmail.com>
2025-06-13 23:35:54 +0700

    Delete back directory

--- back/src/routes/categoryRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getCategories, getArticlesByCategory } from "../controllers/categoryController.js";
-
-const categoryRouter = Router();
-categoryRouter.get("/", getCategories);
-categoryRouter.get("/:id/articles", getArticlesByCategory);
-
-
-export default categoryRouter;
--- back/src/routes/journalistRoutes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { getArticlesByJournalist } from "../controllers/journalistController.js";
-
-const journalistRouter = Router();
-journalistRouter.get("/:id/articles", getArticlesByJournalist);
-
-export default journalistRouter;
